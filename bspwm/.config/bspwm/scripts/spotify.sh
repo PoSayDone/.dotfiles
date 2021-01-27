bbbf6e0f19128f015021b6126b223390eeec8787
Arsenii Kniazev <poseaydone@ya.ru>
2021-01-27 14:42:04 +0500

    Updated some dots

--- bspwm/.config/bspwm/scripts/spotify.sh
+++ bspwm/.config/bspwm/scripts/spotify.sh
@@ -1,89 +1,27 @@
-#!/bin/sh
-#
-# external_rules_command
-#
-# Absolute path to the command used to retrieve rule consequences.
-# The command will receive the following arguments: window ID, class
-# name, instance name, and intermediate consequences. The output of
-# that command must have the following format: key1=value1
-# key2=value2 ...  (the valid key/value pairs are given in the
-# description of the rule command).
-#
-#
-# Rule
-#    General Syntax
-# 	   rule COMMANDS
-#
-#    Commands
-# 	   -a, --add (<class_name>|*)[:(<instance_name>|*)] [-o|--one-shot]
-# 	   [monitor=MONITOR_SEL|desktop=DESKTOP_SEL|node=NODE_SEL]
-# 	   [state=STATE] [layer=LAYER] [split_dir=DIR] [split_ratio=RATIO]
-# 	   [(hidden|sticky|private|locked|marked|center|follow|manage|focus|border)=(on|off)]
-# 	   [rectangle=WxH+X+Y]
-# 		   Create a new rule.
-#
-# 	   -r, --remove
-# 	   ^<n>|head|tail|(<class_name>|*)[:(<instance_name>|*)]...
-# 		   Remove the given rules.
-#
-# 	   -l, --list
-# 		   List the rules.
+#!/bin/bash
 
-border= \
-center= \
-class=$2 \
-desktop= \
-focus= \
-follow= \
-hidden= \
-id=${1?} \
-instance=$3 \
-layer= \
-locked= \
-manage= \
-marked= \
-misc=$4 \
-monitor= \
-node= \
-private= \
-rectangle= \
-split_dir= \
-split_ratio= \
-state= \
-sticky= \
-urgent=;
+wid=$1
+class=$2
+instance=$3
+consequences=$4
 
-spotify() {
-	desktop=^6;
+main() {
+    case "$class" in
+        Spotify)
+            echo "state=pseudo-tiled desktop=6 follow=off focus=off"
+            ;;
+        "")
+            sleep 1.0
+
+            wm_class=($(xprop -id $wid | grep "WM_CLASS" | grep -Po '"\K[^,"]+'))
+
+            class=${wm_class[-1]}
+
+            [[ ${#wm_class[@]} == "2" ]] && instance=${wm_class[0]}
+
+            [[ -n $class ]] && main
+            ;;
+    esac
 }
 
-case $instance.$class in
-	(*.Spotify) spotify;;
-	(.)
-		case $(ps -p "$(xdo pid "$id")" -o comm= 2>/dev/null) in
-			(spotify) spotify;;
-		esac;;
-esac;
-
-printf \
-	${border:+"border=$border"} \
-	${center:+"center=$center"} \
-	${desktop:+"desktop=$desktop"} \
-	${focus:+"focus=$focus"} \
-	${follow:+"follow=$follow"} \
-	${hidden:+"hidden=$hidden"} \
-	${layer:+"layer=$layer"} \
-	${locked:+"locked=$locked"} \
-	${manage:+"manage=$manage"} \
-	${marked:+"marked=$marked"} \
-	${monitor:+"monitor=$monitor"} \
-	${node:+"node=$node"} \
-	${private:+"private=$private"} \
-	${rectangle:+"rectangle=$rectangle"} \
-	${split_dir:+"split_dir=$split_dir"} \
-	${split_ratio:+"split_ratio=$split_ratio"} \
-	${state:+"state=$state"} \
-	${sticky:+"sticky=$sticky"} \
-	${urgent:+"urgent=$urgent"};
-
-# vim: set ft=sh :
+main
